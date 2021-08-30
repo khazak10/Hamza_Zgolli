08de9b546a066f16f669ffb442b19e0832bda3cc
HamzaZg <33738709+khazak10@users.noreply.github.com>
2021-08-30 19:01:14 +0100

    handle exception email

--- Java_assesment/src/main/java/com/recordManagement/Java_assesment/services/SendMailService.java
+++ Java_assesment/src/main/java/com/recordManagement/Java_assesment/services/SendMailService.java
@@ -10,23 +10,29 @@ import com.recordManagement.Java_assesment.repositorys.EmailRepo;
 import com.recordManagement.Java_assesment.servicesImpl.SendMailServiceImpl;
 
 @Service
-public class SendMailService implements SendMailServiceImpl{
+public class SendMailService implements SendMailServiceImpl {
 	@Autowired
 	private EmailRepo emailRepo;
 	@Autowired
 	private JavaMailSender javaMailSender;
-	private String MyEmail ="hamzazgolli@gmail.com";
+	private String MyEmail = "hamzazgolli@gmail.com";
+
 	public SendMailService(EmailRepo emailRepo) {
-		this.emailRepo = emailRepo ;	
+		this.emailRepo = emailRepo;
 	}
-	
+
 	@SuppressWarnings("finally")
-	public EmailEntity  SendMai(EmailEntity email) {
+	public EmailEntity SendMai(EmailEntity email) {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(MyEmail);
 		message.setSubject(email.getSubject());
 		message.setText(email.getMessage());
-		javaMailSender.send(message);
-		return emailRepo.save(email);
+		try {
+			javaMailSender.send(message);
+		} catch (Exception e) {
+			email.setSent(false);
+		} finally {
+			return emailRepo.save(email);
+		}
 	}
 }
